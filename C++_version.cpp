46a2417aa72c583f07fed6e04b501c3f52d95fba
Ishikudeska <83988568+Ishikudeska@users.noreply.github.com>
2023-12-15 11:48:28 -0600

    Update C++_version.cpp

--- C++_version.cpp
+++ C++_version.cpp
@@ -1,141 +1,177 @@
-#include <QApplication>
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QLabel>
-#include <QPushButton>
-#include <QListWidget>
-#include <QInputDialog>
-#include <QMessageBox>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonArray>
+#include <QtWidgets>
 #include <QFile>
-#include <QFileDialog> // Added for QFileDialog
+#include <QTextStream>
 
-class DeliverablesTracker : public QWidget {
-    Q_OBJECT
-
+class Deliverable {
 public:
-    DeliverablesTracker(QWidget *parent = nullptr) : QWidget(parent) {
-        setupUi();
-        loadDeliverables();
-        renderDeliverables();
+    QString name;
+    QString status;
+    QString notes;
+};
 
-        connect(addButton, &QPushButton::clicked, this, &DeliverablesTracker::addDeliverable);
-        connect(exportButton, &QPushButton::clicked, this, &DeliverablesTracker::exportData);
-        connect(importButton, &QPushButton::clicked, this, &DeliverablesTracker::importData); // Connect the import button
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr)
+        : QMainWindow(parent), addButton(this), updateButton(this), saveButton(this), deliverableTable(this) {
+        setWindowTitle("Deliverables Manager");
+
+        setupUI();
+        connectActions();
+        loadFromFile(); // Load existing data when the application starts
     }
 
 private slots:
     void addDeliverable() {
-        // ... (unchanged)
+        // Get user input for the new deliverable
+        QString name = QInputDialog::getText(this, "Add Deliverable", "Enter deliverable name:");
+        if (name.isEmpty()) return;
+
+        // Set the default status to "Not Implemented"
+        Deliverable newDeliverable{ name, "Not Implemented", "" };
+
+        // Add the new deliverable to the list and update the table
+        deliverables.append(newDeliverable);
+        updateTable();
     }
 
-    void exportData() {
-        // ... (unchanged)
+    void updateDeliverable() {
+        // Get the selected row
+        int row = deliverableTable.currentRow();
+        if (row < 0 || row >= deliverables.size()) return;
+
+        // Get user input for the updated deliverable
+        QString name = QInputDialog::getText(this, "Update Deliverable", "Enter updated deliverable name:", QLineEdit::Normal, deliverables[row].name);
+        if (name.isEmpty()) return;
+
+        // Update the deliverable
+        deliverables[row].name = name;
+
+        // Update the table
+        updateTable();
     }
 
-    void importData() {
-        // ... (added functionality, see above)
+    void saveToFile() {
+        // Get the file path for saving
+        QString filePath = QFileDialog::getSaveFileName(this, "Save File", "", "CSV Files (*.csv)");
+        if (filePath.isEmpty()) return;
+
+        // Open the file for writing
+        QFile file(filePath);
+        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            QTextStream stream(&file);
+
+            // Write header
+            stream << "Name,Status,Notes\n";
+
+            // Write deliverables
+            for (const Deliverable& deliverable : deliverables) {
+                stream << deliverable.name << "," << deliverable.status << "," << deliverable.notes << "\n";
+            }
+
+            file.close();
+        }
     }
 
 private:
-    struct Deliverable {
-        QString name;
-        QString status;
-        QString note;
-    };
+    void setupUI() {
+        // Create UI components
+        addButton.setText("Add");
+        updateButton.setText("Update");
+        saveButton.setText("Save");
 
-    QVBoxLayout *layout;
-    QLabel *titleLabel;
-    QListWidget *deliverablesList;
-    QPushButton *addButton;
-    QPushButton *exportButton;
-    QPushButton *importButton; // Added for importing data
+        // Create table headers
+        deliverableTable.setColumnCount(3);
+        QStringList headers = { "Name", "Status", "Notes" };
+        deliverableTable.setHorizontalHeaderLabels(headers);
 
-    QVector<Deliverable> deliverables;
+        // Set up layout
+        QVBoxLayout *mainLayout = new QVBoxLayout;
+        mainLayout->addWidget(&addButton);
+        mainLayout->addWidget(&updateButton);
+        mainLayout->addWidget(&saveButton);
+        mainLayout->addWidget(&deliverableTable);
 
-    void setupUi() {
-        layout = new QVBoxLayout(this);
-        titleLabel = new QLabel("Admin Panel - Deliverables Tracker", this);
-        deliverablesList = new QListWidget(this);
-        addButton = new QPushButton("Add Deliverable", this);
-        exportButton = new QPushButton("Export Data", this);
-        importButton = new QPushButton("Import Data", this); // Added for importing data
-
-        layout->addWidget(titleLabel);
-        layout->addWidget(deliverablesList);
-        layout->addWidget(addButton);
-        layout->addWidget(exportButton);
-        layout->addWidget(importButton); // Added for importing data
-
-        setLayout(layout);
+        QWidget *centralWidget = new QWidget(this);
+        centralWidget->setLayout(mainLayout);
+        setCentralWidget(centralWidget);
     }
 
-    void renderDeliverables() {
-        deliverablesList->clear();
+    void connectActions() {
+        // Connect button clicks to slots
+        connect(deliverableTable.selectionModel(), &QItemSelectionModel::selectionChanged, [this]() {
+            // Enable the "Update" button only when a row is selected
+            updateButton.setEnabled(!deliverableTable.selectedItems().isEmpty());
+        });
 
-        for (const auto &deliverable : deliverables) {
-            QString statusStyle;
-            if (deliverable.status == "implemented")
-                statusStyle = "color: green; background-color: lightgreen;";
-            else if (deliverable.status == "partiallyImplemented")
-                statusStyle = "color: yellow; background-color: lightyellow;";
-            else if (deliverable.status == "notImplemented")
-                statusStyle = "color: red; background-color: lightcoral;";
+        connect(&addButton, &QPushButton::clicked, this, &MainWindow::addDeliverable);
+        connect(&updateButton, &QPushButton::clicked, this, &MainWindow::updateDeliverable);
+        connect(&saveButton, &QPushButton::clicked, this, &MainWindow::saveToFile);
+    }
 
-            QString itemText = QString("<span style=\"%1\">%2 - %3</span>").arg(statusStyle).arg(deliverable.name).arg(deliverable.note);
-            deliverablesList->addItem(itemText);
+    void updateTable() {
+        // Update the table with the current deliverables data
+        deliverableTable.clearContents();
+        deliverableTable.setRowCount(deliverables.size());
+
+        for (int row = 0; row < deliverables.size(); ++row) {
+            const Deliverable& deliverable = deliverables[row];
+
+            QTableWidgetItem *nameItem = new QTableWidgetItem(deliverable.name);
+            QTableWidgetItem *statusItem = new QTableWidgetItem(deliverable.status);
+            QTableWidgetItem *notesItem = new QTableWidgetItem(deliverable.notes);
+
+            deliverableTable.setItem(row, 0, nameItem);
+            deliverableTable.setItem(row, 1, statusItem);
+            deliverableTable.setItem(row, 2, notesItem);
         }
     }
 
-    void storeData() {
-        QFile file("deliverables.json");
-        if (file.open(QIODevice::WriteOnly)) {
-            QJsonArray jsonArray;
-            for (const auto &deliverable : deliverables) {
-                QJsonObject jsonObject;
-                jsonObject["name"] = deliverable.name;
-                jsonObject["status"] = deliverable.status;
-                jsonObject["note"] = deliverable.note;
-                jsonArray.append(jsonObject);
-            }
+    void loadFromFile() {
+        // Load deliverables from a file (if exists)
+        QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "CSV Files (*.csv)");
 
-            QJsonDocument jsonDoc(jsonArray);
-            file.write(jsonDoc.toJson());
-            file.close();
-        } else {
-            QMessageBox::critical(this, "Error", "Unable to open the file for writing.");
-        }
-    }
+        if (filePath.isEmpty()) return;
 
-    void loadDeliverables() {
-        QFile file("deliverables.json");
-        if (file.open(QIODevice::ReadOnly)) {
-            QJsonDocument jsonDoc = QJsonDocument::fromJson(file.readAll());
-            file.close();
+        QFile file(filePath);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream stream(&file);
+            // Skip the header line
+            stream.readLine();
 
-            if (jsonDoc.isArray()) {
-                QJsonArray jsonArray = jsonDoc.array();
-                for (const auto &jsonValue : jsonArray) {
-                    if (jsonValue.isObject()) {
-                        QJsonObject jsonObject = jsonValue.toObject();
-                        Deliverable deliverable;
-                        deliverable.name = jsonObject["name"].toString();
-                        deliverable.status = jsonObject["status"].toString();
-                        deliverable.note = jsonObject["note"].toString();
-                        deliverables.append(deliverable);
-                    }
+            while (!stream.atEnd()) {
+                QString line = stream.readLine();
+                QStringList parts = line.split(',');
+
+                if (parts.size() == 3) {
+                    Deliverable deliverable{
+                        parts[0].trimmed(),
+                        parts[1].trimmed(),
+                        parts[2].trimmed()
+                    };
+
+                    deliverables.append(deliverable);
                 }
             }
+
+            file.close();
+            updateTable();
         }
     }
+
+    QList<Deliverable> deliverables;
+    QPushButton addButton;
+    QPushButton updateButton;
+    QPushButton saveButton;
+    QTableWidget deliverableTable;
 };
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    DeliverablesTracker deliverablesTracker;
-    deliverablesTracker.show();
+
+    MainWindow mainWindow;
+    mainWindow.show();
+
     return app.exec();
 }
 
