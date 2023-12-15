2b62c23a3c53d790df2580a494500bb6424828f4
Ishikudeska <83988568+Ishikudeska@users.noreply.github.com>
2023-12-15 11:58:29 -0600

    Update C++_version.cpp
    final

--- C++_version.cpp
+++ C++_version.cpp
@@ -1,176 +1,153 @@
-#include <QtWidgets>
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QTableWidget>
+#include <QHeaderView>
+#include <QFileDialog>
 #include <QFile>
 #include <QTextStream>
+#include <QInputDialog>
 
-class Deliverable {
-public:
-    QString name;
-    QString status;
-    QString notes;
-};
-
-class MainWindow : public QMainWindow {
+class DeliverablesManager : public QWidget {
     Q_OBJECT
-public:
-    MainWindow(QWidget *parent = nullptr)
-        : QMainWindow(parent), addButton(this), updateButton(this), saveButton(this), deliverableTable(this) {
-        setWindowTitle("Deliverables Manager");
 
-        setupUI();
-        connectActions();
-        loadFromFile(); // Load existing data when the application starts
-    }
+public:
+    DeliverablesManager(QWidget *parent = nullptr);
 
 private slots:
-    void addDeliverable() {
-        // Get user input for the new deliverable
-        QString name = QInputDialog::getText(this, "Add Deliverable", "Enter deliverable name:");
-        if (name.isEmpty()) return;
-
-        // Set the default status to "Not Implemented"
-        Deliverable newDeliverable{ name, "Not Implemented", "" };
-
-        // Add the new deliverable to the list and update the table
-        deliverables.append(newDeliverable);
-        updateTable();
-    }
-
-    void updateDeliverable() {
-        // Get the selected row
-        int row = deliverableTable.currentRow();
-        if (row < 0 || row >= deliverables.size()) return;
-
-        // Get user input for the updated deliverable
-        QString name = QInputDialog::getText(this, "Update Deliverable", "Enter updated deliverable name:", QLineEdit::Normal, deliverables[row].name);
-        if (name.isEmpty()) return;
-
-        // Update the deliverable
-        deliverables[row].name = name;
-
-        // Update the table
-        updateTable();
-    }
-
-    void saveToFile() {
-        // Get the file path for saving
-        QString filePath = QFileDialog::getSaveFileName(this, "Save File", "", "CSV Files (*.csv)");
-        if (filePath.isEmpty()) return;
-
-        // Open the file for writing
-        QFile file(filePath);
-        if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-            QTextStream stream(&file);
-
-            // Write header
-            stream << "Name,Status,Notes\n";
-
-            // Write deliverables
-            for (const Deliverable& deliverable : deliverables) {
-                stream << deliverable.name << "," << deliverable.status << "," << deliverable.notes << "\n";
-            }
-
-            file.close();
-        }
-    }
+    void addDeliverable();
+    void updateDeliverable();
+    void saveToFile();
+    void importFromFile();
+    void updateTable();
+    void cellClicked(int row, int column);
 
 private:
-    void setupUI() {
-        // Create UI components
-        addButton.setText("Add");
-        updateButton.setText("Update");
-        saveButton.setText("Save");
-
-        // Create table headers
-        deliverableTable.setColumnCount(3);
-        QStringList headers = { "Name", "Status", "Notes" };
-        deliverableTable.setHorizontalHeaderLabels(headers);
-
-        // Set up layout
-        QVBoxLayout *mainLayout = new QVBoxLayout;
-        mainLayout->addWidget(&addButton);
-        mainLayout->addWidget(&updateButton);
-        mainLayout->addWidget(&saveButton);
-        mainLayout->addWidget(&deliverableTable);
-
-        QWidget *centralWidget = new QWidget(this);
-        centralWidget->setLayout(mainLayout);
-        setCentralWidget(centralWidget);
-    }
-
-    void connectActions() {
-        // Connect button clicks to slots
-        connect(deliverableTable.selectionModel(), &QItemSelectionModel::selectionChanged, [this]() {
-            // Enable the "Update" button only when a row is selected
-            updateButton.setEnabled(!deliverableTable.selectedItems().isEmpty());
-        });
-
-        connect(&addButton, &QPushButton::clicked, this, &MainWindow::addDeliverable);
-        connect(&updateButton, &QPushButton::clicked, this, &MainWindow::updateDeliverable);
-        connect(&saveButton, &QPushButton::clicked, this, &MainWindow::saveToFile);
-    }
-
-    void updateTable() {
-        // Update the table with the current deliverables data
-        deliverableTable.clearContents();
-        deliverableTable.setRowCount(deliverables.size());
-
-        for (int row = 0; row < deliverables.size(); ++row) {
-            const Deliverable& deliverable = deliverables[row];
-
-            QTableWidgetItem *nameItem = new QTableWidgetItem(deliverable.name);
-            QTableWidgetItem *statusItem = new QTableWidgetItem(deliverable.status);
-            QTableWidgetItem *notesItem = new QTableWidgetItem(deliverable.notes);
-
-            deliverableTable.setItem(row, 0, nameItem);
-            deliverableTable.setItem(row, 1, statusItem);
-            deliverableTable.setItem(row, 2, notesItem);
-        }
-    }
-
-    void loadFromFile() {
-        // Load deliverables from a file (if exists)
-        QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "CSV Files (*.csv)");
-
-        if (filePath.isEmpty()) return;
-
-        QFile file(filePath);
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            QTextStream stream(&file);
-            // Skip the header line
-            stream.readLine();
-
-            while (!stream.atEnd()) {
-                QString line = stream.readLine();
-                QStringList parts = line.split(',');
-
-                if (parts.size() == 3) {
-                    Deliverable deliverable{
-                        parts[0].trimmed(),
-                        parts[1].trimmed(),
-                        parts[2].trimmed()
-                    };
-
-                    deliverables.append(deliverable);
-                }
-            }
-
-            file.close();
-            updateTable();
-        }
-    }
-
-    QList<Deliverable> deliverables;
-    QPushButton addButton;
-    QPushButton updateButton;
-    QPushButton saveButton;
-    QTableWidget deliverableTable;
+    QTableWidget *table;
+    QList<QMap<QString, QString>> deliverables;
 };
+
+DeliverablesManager::DeliverablesManager(QWidget *parent)
+    : QWidget(parent) {
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    QPushButton *addButton = new QPushButton("Add", this);
+    QPushButton *updateButton = new QPushButton("Update", this);
+    QPushButton *saveButton = new QPushButton("Save", this);
+
+    table = new QTableWidget(this);
+    table->setColumnCount(3);
+    table->setHorizontalHeaderLabels({"Name", "Status", "Notes"});
+    table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    connect(addButton, &QPushButton::clicked, this, &DeliverablesManager::addDeliverable);
+    connect(updateButton, &QPushButton::clicked, this, &DeliverablesManager::updateDeliverable);
+    connect(saveButton, &QPushButton::clicked, this, &DeliverablesManager::saveToFile);
+    connect(table, &QTableWidget::cellClicked, this, &DeliverablesManager::cellClicked);
+
+    layout->addWidget(addButton);
+    layout->addWidget(updateButton);
+    layout->addWidget(saveButton);
+    layout->addWidget(table);
+
+    // Load existing data (for demonstration purposes)
+    deliverables.append({{"Name", "Task 1"}, {"Status", "Not Implemented"}, {"Notes", ""}});
+    deliverables.append({{"Name", "Task 2"}, {"Status", "Implemented"}, {"Notes", "Completed on time"}});
+    deliverables.append({{"Name", "Task 3"}, {"Status", "Partially Implemented"}, {"Notes", "In progress"}});
+
+    updateTable();
+}
+
+void DeliverablesManager::addDeliverable() {
+    QString name = QInputDialog::getText(this, "Enter deliverable name", "Name:");
+    if (name.isEmpty())
+        return;
+
+    QMap<QString, QString> newDeliverable = {{"Name", name}, {"Status", "Not Implemented"}, {"Notes", ""}};
+    deliverables.append(newDeliverable);
+    updateTable();
+}
+
+void DeliverablesManager::updateDeliverable() {
+    int row = table->currentRow();
+    if (row < 0)
+        return;
+
+    QString name = QInputDialog::getText(this, "Enter updated deliverable name", "Name:", QLineEdit::Normal,
+                                         deliverables[row]["Name"]);
+    if (name.isEmpty())
+        return;
+
+    deliverables[row]["Name"] = name;
+    updateTable();
+}
+
+void DeliverablesManager::saveToFile() {
+    QString filePath = QFileDialog::getSaveFileName(this, "Save File", "", "CSV Files (*.csv)");
+    if (filePath.isEmpty())
+        return;
+
+    QFile file(filePath);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QTextStream stream(&file);
+        stream << "Name,Status,Notes\n";
+
+        for (const auto &deliverable : deliverables) {
+            stream << deliverable["Name"] << "," << deliverable["Status"] << "," << deliverable["Notes"] << "\n";
+        }
+
+        file.close();
+    }
+}
+
+void DeliverablesManager::importFromFile() {
+    QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "CSV Files (*.csv)");
+    if (filePath.isEmpty())
+        return;
+
+    QFile file(filePath);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream stream(&file);
+        stream.readLine(); // Skip the header
+
+        deliverables.clear();
+        while (!stream.atEnd()) {
+            QString line = stream.readLine();
+            QStringList fields = line.split(',');
+
+            if (fields.size() == 3) {
+                deliverables.append({{"Name", fields[0].trimmed()},
+                                     {"Status", fields[1].trimmed()},
+                                     {"Notes", fields[2].trimmed()}});
+            }
+        }
+
+        file.close();
+        updateTable();
+    }
+}
+
+void DeliverablesManager::updateTable() {
+    table->setRowCount(deliverables.size());
+
+    for (int i = 0; i < deliverables.size(); ++i) {
+        const auto &deliverable = deliverables[i];
+        table->setItem(i, 0, new QTableWidgetItem(deliverable["Name"]));
+        table->setItem(i, 1, new QTableWidgetItem(deliverable["Status"]));
+        table->setItem(i, 2, new QTableWidgetItem(deliverable["Notes"]));
+    }
+}
+
+void DeliverablesManager::cellClicked(int row, int column) {
+    Q_UNUSED(column);
+    table->setCurrentCell(row, 0);
+}
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    MainWindow mainWindow;
-    mainWindow.show();
+    DeliverablesManager deliverablesManager;
+    deliverablesManager.show();
 
     return app.exec();
 }
